bfc25e8a035e83b746bbc6c26ac7c449619c512a
riaz9191 <riazahammed9191@gmail.com>
2024-01-23 15:49:56 +0600

    styles and data added

---
+++ data.js
@@ -0,0 +1,195 @@
+// Sidebar Data
+
+const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "ExpressJS",
+    "MongoDB",
+    "TailwindCSS",
+    "Git",
+    "HTML",
+    "CSS",
+ ];
+ 
+ const details = [
+    { label: "Location", value: "Purana Paltan,Dhaka" },
+    { label: "Experience", value: "> 1+ year" },
+    { label: "Availability", value: "In 1 week" },
+    { label: "Relocation", value: "Yes" },
+ ];
+ 
+ // Navigation
+ 
+ const navigation = [
+    { name: "Resume", href: "./" },
+    { name: "Projects", href: "./projects.html" },
+    { name: "Blog", href: "./blog.html" },
+ ];
+ 
+ /**
+  * Resume Page
+  */
+ 
+ // Experience Data
+ 
+ const jobsList = [
+    {
+       icon: "./assets/images/aia.png",
+       role: "Web Developer ",
+       company: "ALL IN ALL BD CORPORATION",
+       location: "Farmgate,Dhaka",
+       period: "Aug 2023 - Present",
+       type: "Intern",
+       description:
+          `Maintained and optimized company website for optimal user experience and implemented
+          SEO strategies.Developed and managed a WordPress e-commerce website for seamless online shopping.Enhanced company branding through logo and graphics design initiatives.`,
+    },
+    // {
+    //    icon: "./assets/images/amazon.jpg",
+    //    role: "Software Engineer Intern",
+    //    company: "Amazon",
+    //    location: "Seattle, WA",
+    //    period: "Feb 2018 - Jun 2020",
+    //    type: "Full-time",
+    //    description:
+    //       "Designed and implemented new features for Amazon's e-commerce platform. Worked on a team of engineers to improve the user experience and increase sales.",
+    // },
+    // {
+    //    icon: "./assets/images/microsoft.jpg",
+    //    role: "Software Development Engineer in Test",
+    //    company: "Microsoft",
+    //    location: "Redmond, WA",
+    //    period: "Aug 2017 - Jan 2018",
+    //    type: "Full-time",
+    //    description:
+    //       "Wrote and executed test cases for Microsoft's Windows operating system. Worked with a team of engineers to ensure the quality of the software.",
+    // },
+    // {
+    //    icon: "./assets/images/tesla.jpg",
+    //    role: "Software Engineer Intern",
+    //    company: "Tesla",
+    //    location: "Palo Alto, CA",
+    //    period: "Dec 2016 - May 2017",
+    //    type: "Full-time",
+    //    description:
+    //       "Developed software for Tesla's autonomous driving vehicles. Worked on a team of engineers to improve the safety and reliability of the software.",
+    // },
+ ];
+ 
+ // Education
+ 
+ const educationList = [
+    {
+       icon: "./assets/images/diu.jpg",
+       degree: "Bachelor in Computer Science and Engineering (B. Sc in C.S.E)",
+       university: "Daffodil International University,",
+       location: "Dhaka,Bangladesh",
+       period: "2018 - 2022",
+       description:
+          `Result : CGPA 3.66 out of 4.00 `,
+    },
+    {
+       icon: "./assets/images/ahc.jpg",
+       degree: "Higher Secondary Certificate (HSC)",
+       university: "Azizul Haque Collage, Bogura",
+       location: "Bogura,Rajshahi",
+       period: "2016 - 2018",
+       description:
+          "Result : GPA 5.00 out of 5.00",
+    },
+    {
+       icon: "./assets/images/biam.png",
+       degree: "Associate of Science in Computer Science",
+       university: "Biam Model School and Collage",
+       location: "Bogura,Rajshahi",
+       period: "2012 - 2014",
+       description:
+          "Result : GPA 5.00 out of 5.00",
+    },
+ ];
+ 
+ /**
+  * Projects Page
+  */
+ 
+ // Projects
+ const projectsList = [
+    {
+       icon: "./assets/images/school.png",
+       name: "ArtOne",
+       category: "School Platform",
+       role: "Designer & Developer",
+       date: "June 2023",
+       description:
+          "Full-stack summer camp school platform with MERN stack. Robust dashboard system for efficient management. Differentiated user logins for administrators, teachers, and students.",
+       website: "https://art-one-7b01d.web.app/",
+    },
+    {
+       icon: "./assets/images/wooden.png",
+       name: "Wooden Toy Market Place",
+       category: "Toy Market",
+       role: "Designer & Developer",
+       date: "May 2023",
+       description:
+          "Developed a Full Stack Market Place Using MERN. Improved User Experience with a stunning look. Google authentication for secure login. Private Routes for logged in user only.",
+       website: "https://wooden-feb93.web.app/",
+    },
+    // {
+    //    icon: "./assets/images/cloudsnip.jpg",
+    //    name: "CloudSnip",
+    //    category: "Cloud Services",
+    //    role: "Designer & Developer",
+    //    date: "May 2020",
+    //    description:
+    //       "CloudSnip is a versatile cloud storage service, providing users with secure file storage, sharing, and synchronization across devices. Experience the next level of cloud convenience.",
+    //    website: "https://cloudsnip.net",
+    // },
+    // {
+    //    icon: "./assets/images/healthtrackr.jpg",
+    //    name: "HealthTrackr",
+    //    category: "HealthTech",
+    //    role: "Founder",
+    //    date: "Oct 2023",
+    //    description:
+    //       "HealthTrackr revolutionizes personal health management, offering a comprehensive platform for tracking fitness, nutrition, and health metrics. Take control of your well-being.",
+    //    website: "https://healthtrackr.app",
+    // },
+ ];
+ 
+ /**
+  * Projects Page
+  */
+ 
+ // Posts
+ 
+ const posts = [
+    {
+       date: "February 22, 2024",
+       title: "Building an Online Presence as a Full Stack Developer",
+       excerpt:
+          "Explore the importance of establishing a strong online presence for full stack developers. Learn how to create a compelling portfolio, engage with the developer community, and leverage social media to boost your career.",
+    },
+    {
+       date: "February 15, 2024",
+       title: "Navigating the Full Stack Developer Job Interview Maze",
+       excerpt:
+          "Prepare for success in your full stack developer job interviews. Uncover common interview questions, tips for showcasing your problem-solving skills, and strategies for handling technical assessments.",
+    },
+    {
+       date: "February 8, 2024",
+       title: "Mastering the Art of Full Stack Developer Cover Letters",
+       excerpt:
+          "Explore the art of writing compelling cover letters for full stack developer positions. Learn how to tailor your cover letter to showcase your technical prowess and demonstrate your passion for coding.",
+    },
+    {
+       date: "February 1, 2024",
+       title: "Crafting a Comprehensive Full Stack Developer Resume",
+       excerpt:
+          "Learn the essential elements to include in your full stack developer resume for a stand-out application. Nail down the perfect balance between showcasing skills and work experience.",
+    },
+ ];
+ 
+
+
+ 
